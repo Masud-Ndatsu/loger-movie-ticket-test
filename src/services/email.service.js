1721877c2c8d083fe6ad24441cb3e4f6927c59ef
Masud Ndatsu <masudndatsu@gmail.com>
2024-08-29 14:02:00 +0100

    Made some changes

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendConfirmationEmail = async (ticket) => {
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
+    service: "gmail",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
@@ -11,7 +13,7 @@ const sendConfirmationEmail = async (ticket) => {
 
   const mailOptions = {
     from: process.env.EMAIL_USERNAME,
-    to: `${ticket.user.phone_number}@your_email_provider.com`,
+    to: `${ticket.user.phone_number}@gmail.com`,
     subject: "Ticket Booking Confirmation",
     text: `Your ticket for ${ticket.movies[0].name} has been booked successfully.`,
   };
